50274bfed15de65e9c9d3a68bdd03305a26e230f
kamalus <formation.kamal.haddou@gmail.com>
2022-03-02 17:11:34 +0100

    mise ne place de vueX (non finit)

---
+++ src/components/NavBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="menu d-flex align-items-center justify-content-around">
+      <nav>
+        <router-link to="/"
+          ><div class="btn btn-primary m-2 fs-5">Acceuil</div></router-link
+        >
+        <router-link to="/favoris"
+          ><div class="btn btn-primary m-2 fs-5">Favoris</div></router-link
+        >
+        <router-link to="/a-voir"
+          ><div class="btn btn-primary m-2 fs-5">
+            Ma liste de films
+          </div></router-link
+        >
+      </nav>
+      <div>
+        <input
+          class="input"
+          v-on:keydown.enter="SearchMovie"
+          placeholder="Entrez le nom d'un film"
+          type="text"
+          id="query"
+        />
+        <input
+          class="btn btn-primary"
+          v-on:click="SearchMovie"
+          type="submit"
+          value="Rechercher un film"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavBar",
+  methods: {
+    SearchMovie: function () {
+      this.$store.commit("setQuery", document.getElementById("query").value);
+
+      //action : dispatch
+
+      //   fetch(
+      //     "https://api.themoviedb.org/3/search/movie?api_key=16339e3d2f16c3253b083ac43d403e38&page=1&query=" +
+      //       this.search
+      //   )
+      //     .then((response) => response.json())
+      //     .then((movies) => {
+      //       console.log(movies);
+      //       this.movies = movies.results;
+      //     })
+      //     .catch((e) => {
+      //       console.error("ERREUR", e);
+      //     });
+    },
+  },
+};
+</script>
--- src/views/WatchList.vue
+++ src/views/WatchList.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
+    <NavBar></NavBar>
     <h1>Ma liste</h1>
-    <div class="menu d-flex align-items-center justify-content-around">
-      <nav class="menu">
-        <router-link to="/"
-          ><div class="btn btn-primary m-2 fs-5">Acceuil</div></router-link
-        >
-        <router-link to="/favoris"
-          ><div class="btn btn-primary m-2 fs-5">Favoris</div></router-link
-        >
-        <router-link to="/a-voir"
-          ><div class="btn btn-primary m-2 fs-5">Ma liste</div></router-link
-        >
-      </nav>
-    </div>
   </div>
 </template>
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "WatchList",
+  components: {
+    NavBar,
+  },
+};
+</script>
 <style scoped>
 h1 {
   color: white;
